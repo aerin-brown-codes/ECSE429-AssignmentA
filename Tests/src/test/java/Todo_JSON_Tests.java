5caf7dbf5a7283b9582d5fc2336c8172dc805925
Aerin Brown <72281505+aerin-brown-codes@users.noreply.github.com>
2025-02-18 21:24:47 -0500

    feat: todo happy path tests

--- Tests/src/test/java/Todo_JSON_Tests.java
+++ Tests/src/test/java/Todo_JSON_Tests.java
@@ -2,9 +2,7 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -161,6 +159,7 @@ public class Todo_JSON_Tests {
         assertEquals("{\"categories\":[{\"id\":\"1\",\"title\":\"Office\",\"description\":\"\"}]}", result);
     }
 
+
     @Test
     public void testCreateTodo() throws MalformedURLException, IOException {
         // arrange
@@ -168,7 +167,7 @@ public class Todo_JSON_Tests {
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestProperty("Content-Type", "application/json");
         con.setRequestMethod("POST");
-        con.setDoOutput(true); 
+        con.setDoOutput(true);
         String jsonInputString = "{\"title\":\"test\",\"doneStatus\":false,\"description\":\"test\"}";
         try(OutputStream os = con.getOutputStream()) {
             byte[] input = jsonInputString.getBytes("utf-8");
@@ -191,5 +190,237 @@ public class Todo_JSON_Tests {
         // assert
         assertEquals(201, status);
         assertEquals("{\"id\":\"3\",\"title\":\"test\",\"doneStatus\":\"false\",\"description\":\"test\"}", result);
+    }
+
+    @Test
+    public void testUpdateTodoPost() throws MalformedURLException, IOException {
+        // arrange
+        URL url = new URL(BASE_URL + "/1");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+        String jsonInputString = "{\"title\":\"test\",\"doneStatus\":true,\"description\":\"test\"}";
+        try(OutputStream os = con.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        // act
+        int status = con.getResponseCode();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        String result = content.toString();
+        con.disconnect();
+
+        // assert
+        assertEquals(200, status);
+        assertEquals("{\"todos\":[{\"id\":\"1\",\"title\":\"test\",\"doneStatus\":\"true\",\"description\":\"test\",\"tasksof\":[{\"id\":\"1\"}],\"categories\":[{\"id\":\"1\"}]}]}", result);
+    }
+
+    @Test
+    public void testUpdateTodoPut() throws MalformedURLException, IOException {
+        // arrange
+        URL url = new URL(BASE_URL + "/1");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+        String jsonInputString = "{\"title\":\"test\",\"doneStatus\":true,\"description\":\"test\"}";
+        try(OutputStream os = con.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        // act
+        int status = con.getResponseCode();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        String result = content.toString();
+        con.disconnect();
+
+        // assert
+        assertEquals(200, status);
+        assertEquals("{\"id\":\"1\",\"title\":\"test\",\"doneStatus\":\"true\",\"description\":\"test\"}", result);
+    }
+
+    @Test
+    public void testDeleteTodo() throws MalformedURLException, IOException {
+        // arrange
+        URL url = new URL(BASE_URL + "/1");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("DELETE");
+
+        // act
+        int status = con.getResponseCode();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        String result = content.toString();
+        con.disconnect();
+
+        // assert
+        assertEquals(200, status);
+    }
+
+    @Test
+    public void testDeleteTodoProjectRelationship() throws MalformedURLException, IOException {
+        // arrange
+        URL url = new URL(BASE_URL + "/1/tasksof/1");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("DELETE");
+
+        // act
+        int status = con.getResponseCode();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        String result = content.toString();
+        con.disconnect();
+
+        // assert
+        assertEquals(200, status);
+    }
+
+    @Test
+    public void testDeleteTodoCategoryRelationship() throws MalformedURLException, IOException {
+        // arrange
+        URL url = new URL(BASE_URL + "/1/categories/1");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("DELETE");
+
+        // act
+        int status = con.getResponseCode();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        String result = content.toString();
+        con.disconnect();
+
+        // assert
+        assertEquals(200, status);
+    }
+
+    @Test
+    public void testHeadAllTodos() throws MalformedURLException, IOException {
+        // arrange
+        URL url = new URL(BASE_URL);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("HEAD");
+
+        // act
+        int status = con.getResponseCode();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        String result = content.toString();
+        con.disconnect();
+
+        // assert
+        assertEquals(200, status);
+    }
+
+    @Test
+    public void testHeadTodo() throws MalformedURLException, IOException {
+        // arrange
+        URL url = new URL(BASE_URL + "/1");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("HEAD");
+
+        // act
+        int status = con.getResponseCode();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        String result = content.toString();
+        con.disconnect();
+
+        // assert
+        assertEquals(200, status);
+    }
+
+    @Test
+    public void testHeadTodoProjects() throws MalformedURLException, IOException {
+        // arrange
+        URL url = new URL(BASE_URL + "/1/tasksof");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("HEAD");
+
+        // act
+        int status = con.getResponseCode();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        String result = content.toString();
+        con.disconnect();
+
+        // assert
+        assertEquals(200, status);
+    }
+
+    @Test
+    public void testHeadTodoCategories() throws MalformedURLException, IOException {
+        // arrange
+        URL url = new URL(BASE_URL + "/1/categories");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("HEAD");
+
+        // act
+        int status = con.getResponseCode();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        String result = content.toString();
+        con.disconnect();
+
+        // assert
+        assertEquals(200, status);
     }
 }
